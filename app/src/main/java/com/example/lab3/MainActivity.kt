bf3916330eab51a3088eda912cacf459097cbe87
Bulochka56 <bul.xajrullin@yandex.ru>
2023-11-02 17:11:48 +0300

    added basic functionality

--- app/src/main/java/com/example/lab3/MainActivity.kt
+++ app/src/main/java/com/example/lab3/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.lab3
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var myImageView: ImageView
+    private var clickCount = 0
+    private lateinit var textCount: TextView
+    private lateinit var linearLayout: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        myImageView = findViewById(R.id.imageView)
+        linearLayout = findViewById(R.id.LinearLayout)
+        textCount = findViewById(R.id.textView)
+
+        myImageView.setOnClickListener(View.OnClickListener {
+            clickCount++
+            textCount.text = clickCount.toString()
+            clickCheck()
+        })
+    }
+    private fun clickCheck(){
+        when (clickCount) {
+            3 -> textCount.setTypeface(null,Typeface.BOLD_ITALIC)
+            5 -> linearLayout.setBackgroundColor(resources.getColor(R.color.lime))
+            7 -> textCount.textSize = 48f
+        }
     }
 }
