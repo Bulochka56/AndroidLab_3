0ef97d073e751e014c02c31a56c877271b0d8ac1
Bulochka56 <bul.xajrullin@yandex.ru>
2023-11-02 17:46:28 +0300

    add clicker function

--- app/src/main/java/com/example/lab3/MainActivity.kt
+++ app/src/main/java/com/example/lab3/MainActivity.kt
@@ -7,30 +7,55 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.LinearLayout
+import java.util.Timer
+import java.util.TimerTask
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var myImageView: ImageView
-    private var clickCount = 0
+    private var clickCount = 0.0
+    private var upgradeCount = 0.0
     private lateinit var textCount: TextView
-    private lateinit var linearLayout: LinearLayout
+    private lateinit var linearLayoutMain: LinearLayout
+    private lateinit var linearLayoutUpgrade: LinearLayout
+    private lateinit var textUpgrade: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         myImageView = findViewById(R.id.imageView)
-        linearLayout = findViewById(R.id.LinearLayout)
+        linearLayoutMain = findViewById(R.id.LinearLayout)
         textCount = findViewById(R.id.textView)
+        textUpgrade = findViewById(R.id.textUpgrade)
+        linearLayoutUpgrade = findViewById(R.id.linearLayoutUp)
 
         myImageView.setOnClickListener(View.OnClickListener {
             clickCount++
             textCount.text = clickCount.toString()
             clickCheck()
         })
+        linearLayoutUpgrade.setOnClickListener(View.OnClickListener {
+            if(clickCount >= 15.0 ){
+                upgradeCount +=0.5
+                clickCount-=15.0
+                textUpgrade.text = "$upgradeCount cookie/s"
+                textCount.text = clickCount.toString()
+            }
+        })
+        val timer = Timer()
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                clickCount+=upgradeCount
+                runOnUiThread {
+                    textCount.text = clickCount.toString()
+                }
+            }
+        }, 1000, 1000)
     }
     private fun clickCheck(){
         when (clickCount) {
-            3 -> textCount.setTypeface(null,Typeface.BOLD_ITALIC)
-            5 -> linearLayout.setBackgroundColor(resources.getColor(R.color.lime))
-            7 -> textCount.textSize = 48f
+            3.0 -> textCount.setTypeface(null,Typeface.BOLD_ITALIC)
+            5.0 -> linearLayoutMain.setBackgroundColor(resources.getColor(R.color.lime))
+            7.0 -> textCount.textSize = 48f
         }
     }
 }
